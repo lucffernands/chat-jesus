4b6e2e43b7d54b7a34bd6910a06fbfb8af60ae1c
Luciano Fernandes <93296181+lucffernands@users.noreply.github.com>
2025-08-10 15:17:20 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -179,3 +179,9 @@ window.onload = () => {
     speechSynthesis.onvoiceschanged = () => {};
   };
 };
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js")
+    .then(() => console.log("Service Worker registrado com sucesso."))
+    .catch(err => console.error("Erro ao registrar Service Worker:", err));
+}
