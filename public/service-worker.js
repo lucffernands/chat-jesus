19f45f4ff0ef4322069e4447d9e8326aad4dbb8b
Luciano Fernandes <93296181+lucffernands@users.noreply.github.com>
2025-08-11 07:07:29 -0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,39 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('meu-app-cache').then(cache => {
-      return cache.addAll(['/', '/index.html']);
+const CACHE_NAME = "jesus-chat-v1";
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/manifest.json",
+  "/icons/pray-128x.png",
+  "/icons/pray-512x.png"
+];
+
+// Instala o Service Worker e armazena os arquivos no cache
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => {
-      return resp || fetch(e.request);
+// Ativa o SW e remove caches antigos
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Intercepta as requisições e serve do cache se disponível
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
